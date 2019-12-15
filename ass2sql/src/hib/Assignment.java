d46765afdf1a499e89991503a11c4daf7cc33c73
Harel <harels55@gmail.com>
2019-12-15 19:40:29 +0200

    first

---
+++ ass2sql/src/hib/Assignment.java
@@ -0,0 +1,33 @@
+package hib;
+
+import java.util.List;
+
+public class Assignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static boolean isExistUsername (String username){
+		return true ; 
+	} 
+	public static String insertUser(String username, String password, String first_name, String last_name, String day_of_birth, String month_of_birth, String year_of_birth){
+		return "" ; 
+	}
+	
+	public static List<Mediaitems> getTopNItems (int top_n){
+		return  null ; 
+	}
+	
+	public static String validateUser (String username, String password)
+	{
+		return "" ; 
+	}
+	
+	public static String validateAdministrator (String username, String password)
+	{
+		return "" ; 
+	} 
+	
+
+}
